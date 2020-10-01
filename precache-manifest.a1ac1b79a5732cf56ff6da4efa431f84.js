5b27f5348d6a26ece0b0515be7144de2863e26fb
oranje322 <oranje322@mail.ru>
2020-10-01 17:28:03 +0300

    Updates

---
+++ precache-manifest.a1ac1b79a5732cf56ff6da4efa431f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5812f1f4293331d625e0ad0cd7f43ce",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "04884ec5baf87889a5fc",
+    "url": "/react-todo-app/static/css/main.a8c86875.chunk.css"
+  },
+  {
+    "revision": "da5c68a6e83736e63fb1",
+    "url": "/react-todo-app/static/js/2.c16bf901.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-todo-app/static/js/2.c16bf901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04884ec5baf87889a5fc",
+    "url": "/react-todo-app/static/js/main.4adf344b.chunk.js"
+  },
+  {
+    "revision": "77ef626dbdd4b1e53296",
+    "url": "/react-todo-app/static/js/runtime-main.3db00947.js"
+  },
+  {
+    "revision": "d80485304bdd140fe6254947524597b3",
+    "url": "/react-todo-app/static/media/oHPNYn1wymI.d8048530.jpg"
+  }
+]);
