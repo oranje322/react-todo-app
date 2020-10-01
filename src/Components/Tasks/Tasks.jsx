6587e931f070b99a2462a5f058c6a4ac62ef2c44
oranje322 <oranje322@mail.ru>
2020-10-01 16:34:18 +0300

    toDeploy

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './Tasks.scss'
 import TaskItem from "./TaskItem";
 
 import {useDispatch, useSelector} from "react-redux";
-import {checkTask, setTasks, showAllTasks} from "../../Redux/Reducers/reducer";
+import {checkTask, deleteTask, editItemList, editTask, setTasks} from "../../Redux/Reducers/reducer";
+import EmptyTasks from "./EmptyTasks";
 
 const Tasks = () => {
 
@@ -14,17 +15,24 @@ const Tasks = () => {
 
 
     const dispatch = useDispatch()
+
+
     const activeListItem = useSelector(state => state.activeListItem)
 
-    const tasksItems = useSelector(state => state.items[activeListItem].tasks)
-
     const isShowAllTasks = useSelector(state => state.isShowAllTasks)
+    const allTasks = useSelector(state => state.showAllTasks)
+    const sidebarList = useSelector(state => state.items)
 
-    const showAllTasks = useSelector(state => state.showAllTasks)
 
-    // const sidebarList = useSelector(state => state.items)
-    //
-    // const tasksLength = sidebarList.length === 0 ? false : true
+    const tasksList = sidebarList[activeListItem] ? sidebarList[activeListItem].tasks : []
+    const titleName = sidebarList[activeListItem] ? sidebarList[activeListItem].name : 'Задачи отсутствуют'
+
+    const [titleNameInput, setTitleNameInput] = useState(titleName)
+    const [visibleTitleNameInput, setVisibleTitleNameInput] = useState(false)
+
+    useEffect(() => {
+        setTitleNameInput(titleName)
+    }, [titleName])
 
 
     const changeInputValue = (e) => {
@@ -44,7 +52,7 @@ const Tasks = () => {
         }
         const obj = {
             listItem: activeListItem,
-            id: tasksItems.length,
+            id: tasksList.length,
             task: inputValue,
             isCompleted: false
         }
@@ -63,44 +71,91 @@ const Tasks = () => {
         setVisibleInput(false)
     }
 
-    const onCheckTask = (id) => {
+    const onCheckTask = (zxc) => {
         const obj = {
-            taskId: id,
-            activeListItem
+            taskId: zxc.id,
+            activeListItem,
+            wasRender: zxc.wasRender
         }
         dispatch(checkTask(obj))
 
     }
 
+    const onDeleteTaskItem = (payload) => {
+
+        dispatch(deleteTask(payload))
+    }
+
+    const onEditTask = payload => {
+        dispatch(editTask(payload))
+    }
+
+    const onChangeTitleName = (e) => {
+        setTitleNameInput(e.target.value)
+    }
+
+    const openTitleNameInput = () => {
+        setVisibleTitleNameInput(true)
+    }
+
+    const sendTitleNameInputToRedux = () => {
+        const payload = {
+            id: sidebarList[activeListItem].id,
+            name: titleNameInput,
+            color: sidebarList[activeListItem].color,
+            tasks: sidebarList[activeListItem].tasks
+        }
+        console.log(payload)
+
+        dispatch(editItemList(payload))
+        setVisibleTitleNameInput(false)
+    }
 
 
 
     return (
         <div className={'todo__tasks'}>
+
             <div className="tasks__title">
-                <h2>Покупки</h2>
-                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 12.0504V14.5834C0 14.8167 0.183308 15 0.41661 15H2.9496C3.05792 15 3.16624 14.9583 3.24123 14.875L12.34 5.78458L9.21542 2.66001L0.124983 11.7504C0.0416611 11.8338 0 11.9338 0 12.0504ZM14.7563 3.36825C14.8336 3.29116 14.8949 3.1996 14.9367 3.0988C14.9785 2.99801 15 2.88995 15 2.78083C15 2.6717 14.9785 2.56365 14.9367 2.46285C14.8949 2.36205 14.8336 2.27049 14.7563 2.19341L12.8066 0.24367C12.7295 0.166428 12.6379 0.105146 12.5372 0.0633343C12.4364 0.021522 12.3283 0 12.2192 0C12.1101 0 12.002 0.021522 11.9012 0.0633343C11.8004 0.105146 11.7088 0.166428 11.6318 0.24367L10.107 1.76846L13.2315 4.89304L14.7563 3.36825Z"/>
-                </svg>
+                { (visibleTitleNameInput &&
+                    <input value={titleNameInput} onChange={(e) => onChangeTitleName(e)} onBlur={sendTitleNameInputToRedux} className={'changeTaskInput'} autoFocus type="text"/>) ||
+                <h2>{titleName}</h2>
+                }
+                { (activeListItem !== 0) &&
+                    <div onClick={openTitleNameInput}>
+                        <svg onClick={openTitleNameInput} width="15" height="15" viewBox="0 0 15 15" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M0 12.0504V14.5834C0 14.8167 0.183308 15 0.41661 15H2.9496C3.05792 15 3.16624 14.9583 3.24123 14.875L12.34 5.78458L9.21542 2.66001L0.124983 11.7504C0.0416611 11.8338 0 11.9338 0 12.0504ZM14.7563 3.36825C14.8336 3.29116 14.8949 3.1996 14.9367 3.0988C14.9785 2.99801 15 2.88995 15 2.78083C15 2.6717 14.9785 2.56365 14.9367 2.46285C14.8949 2.36205 14.8336 2.27049 14.7563 2.19341L12.8066 0.24367C12.7295 0.166428 12.6379 0.105146 12.5372 0.0633343C12.4364 0.021522 12.3283 0 12.2192 0C12.1101 0 12.002 0.021522 11.9012 0.0633343C11.8004 0.105146 11.7088 0.166428 11.6318 0.24367L10.107 1.76846L13.2315 4.89304L14.7563 3.36825Z"/>
+                        </svg>
+                    </div>
+                }
+
 
 
             </div>
 
             <div className="tasks__list">
 
-                {  isShowAllTasks ? showAllTasks.map((task, index) => <TaskItem data={task} key={'task_'+ index}
-                                                                               onCheckTask={onCheckTask}/>)
-                    :
-                    tasksItems.map((task, index) => <TaskItem data={task} key={'task_'+ index}
-                                                              onCheckTask={onCheckTask}/>)}
+                {  (isShowAllTasks && allTasks.length >= 1) ? allTasks.map((task, index) => <TaskItem data={task} key={'task_'+ index}
+                                                                               onCheckTask={onCheckTask} wasRender={task.listItem}
+                                                                                                      onDeleteTaskItem={onDeleteTaskItem}
+                                                                                                      onEditTask={onEditTask}/>) :
+
+                    (tasksList.length >= 1) ? tasksList.map((task, index) => <TaskItem data={task} key={'task_'+ index}
+                                                              onCheckTask={onCheckTask} wasRender={task.listItem}
+                                                                                       onDeleteTaskItem={onDeleteTaskItem}
+                                                                                       onEditTask={onEditTask}/>) :
+                     <EmptyTasks/>
+                }
 
 
-                { visibleInput &&
-                    <input onChange={ (e) => changeInputValue(e)} value={inputValue} className={'field'} type="text" placeholder={'Текст задачи'}/>
+                { (visibleInput && activeListItem !== 0) &&
+                    <input onChange={ (e) => changeInputValue(e)} value={inputValue} className={'field newTaskInput'} type="text" placeholder={'Текст задачи'}/>
                 }
             </div>
             <div className="newTask">
-                { visibleAddTaskBtn &&
+                { (visibleAddTaskBtn && sidebarList.length >1 && activeListItem !== 0) &&
                     <button onClick={onClickAddTask}>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8 1V15" stroke="#B4B4B4" strokeWidth="2" strokeLinecap="round"
@@ -112,7 +167,7 @@ const Tasks = () => {
                     </button>
                 }
                 {
-                    visibleSendTaskBtn &&
+                    (visibleSendTaskBtn && activeListItem !== 0) &&
                         <div className={'sendTaskBtn'}>
                             <button onClick={clickToDispatch} className={'greenBtn'}>Добавить задачу</button>
                             <button onClick={onClickCloseBtn} className={'grayBtn'}>Отмена</button>
--- src/Redux/Reducers/reducer.js
+++ src/Redux/Reducers/reducer.js
@@ -1,33 +1,43 @@
 const SET_ITEMS_LIST = 'SET_ITEMS_LIST'
+const EDIT_ITEM_LIST = 'EDIT_ITEM_LIST'
 const DELETE_ITEM_LIST = 'DELETE_ITEM_LIST'
 const SET_ACTIVE_ITEM_LIST = 'SET_ACTIVE_ITEM_LIST'
 
 const SET_TASKS = 'SET_TASKS'
+const EDIT_TASK = 'EDIT_TASK'
+const DELETE_TASK = 'DELETE_TASK'
 const CHECK_TASK = 'CHECK_TASK'
 
 const SHOW_ALL_TASKS = 'SHOW_ALL_TASKS'
 
 const initialState = {
-    items: [{id: 0, name: 'Покупки', color: 'green', tasks: [
-            {listItem: 0, id: 0, task: 'Купить колбасы', isCompleted: true},
-            {listItem: 0, id: 1, task: 'Купить молока', isCompleted: false}
+    items: [{id: 0, name: 'Все задачи', color: '', tasks: []},
+        {id: 1, name: 'Покупки', color: 'green', tasks: [
+            {listItem: 1, id: 0, task: 'Купить колбасы', isCompleted: true},
+            {listItem: 1, id: 1, task: 'Купить молока', isCompleted: false}
         ]},
-        {id: 1, name: 'Книги', color: 'orange', tasks: [
-                {listItem: 1, id: 0, task: 'Прочитать Гарри Поттера', isCompleted: true}
+        {id: 2, name: 'Книги', color: 'orange', tasks: [
+                {listItem: 2, id: 0, task: 'Прочитать Гарри Поттера', isCompleted: true}
             ]},
-        {id: 2, name: 'Сериалы', color: 'red', tasks : [
-                {listItem: 2, id: 0, task: 'Посмотреть House M.D.', isCompleted: false}
+        {id: 3, name: 'Сериалы', color: 'red', tasks : [
+                {listItem: 3, id: 0, task: 'Посмотреть House M.D.', isCompleted: false}
             ]},
-        {id: 3, name: 'Учеба', color: 'blue', tasks: [
-                {listItem: 3, id: 0, task: 'Выучить JS', isCompleted: true},
-                {listItem: 3, id: 1, task: 'Выучить React', isCompleted: true},
+        {id: 4, name: 'Учеба', color: 'blue', tasks: [
+                {listItem: 4, id: 0, task: 'Выучить JS', isCompleted: true},
+                {listItem: 4, id: 1, task: 'Выучить React', isCompleted: true},
             ]},
-        {id: 4, name: 'Разное', color: 'indigo', tasks: [
-                {listItem: 4, id: 0, task: 'Убраться в комнате', isCompleted: false},
+        {id: 5, name: 'Разное', color: 'indigo', tasks: [
+                {listItem: 5, id: 0, task: 'Убраться в комнате', isCompleted: false},
             ]}],
-    activeListItem: 0,
+    activeListItem: 1,
     isShowAllTasks: false,
-    showAllTasks: []
+    showAllTasks: [{listItem: 1, id: 0, task: "Купить колбасы", isCompleted: true},
+{listItem: 1, id: 1, task: "Купить молока", isCompleted: false},
+{listItem: 2, id: 0, task: "Прочитать Гарри Поттера", isCompleted: true},
+{listItem: 3, id: 0, task: "Посмотреть House M.D.", isCompleted: false},
+{listItem: 4, id: 0, task: "Выучить JS", isCompleted: true},
+{listItem: 4, id: 1, task: "Выучить React", isCompleted: true},
+{listItem: 5, id: 0, task: "Убраться в комнате", isCompleted: false}]
 }
 
 
@@ -38,22 +48,42 @@ export const reducer = (state = initialState, action) => {
                 ...state,
                 items: [...state.items, action.payload]
             }
+        case EDIT_ITEM_LIST:
+        return  {
+            ...state,
+            items: state.items.map(item => {
+                    if(item.id === action.payload.id) {
+                        const newItem = {
+                            id: action.payload.id,
+                            name: action.payload.name,
+                            color: action.payload.color,
+                            tasks: action.payload.tasks
+                        }
+                        return newItem
+                    } else {
+                        return item
+                    }
+                }
+            )
+        }
+
         case DELETE_ITEM_LIST: {
-            let newItems = state.items.filter(obj => obj.id != action.payload)
-            if(newItems.length === 0){
-                newItems = [{task: []}]
-            }
+            let newItems = state.items.filter(obj => obj.id !== action.payload)
+
 
             return {
                 ...state,
-                items: newItems
+                items: newItems,
+                showAllTasks: [...newItems.map(task => task.tasks)].flat()
             }
         }
         case SET_ACTIVE_ITEM_LIST:
+
             return {
                 ...state,
                 activeListItem: action.payload,
-                isShowAllTasks: false,
+                isShowAllTasks: action.payload === 0 ? true : false
+
 
             }
         case SET_TASKS: {
@@ -72,39 +102,104 @@ export const reducer = (state = initialState, action) => {
                 })
             }
         }
-        case CHECK_TASK:
-            return {
-                ...state,
-                items: state.items.map(item => {
-                    if(item.id !== action.payload.activeListItem) {
-                        return item
-                    } else {
-                        const newTasksMass = item.tasks.map(task => {
-                            if(task.id === action.payload.taskId) {
-                                return {
-                                    listItem: action.payload.activeListItem,
-                                    id: task.id,
-                                    task: task.task,
-                                    isCompleted: !task.isCompleted
+
+        case EDIT_TASK: {
+            const newItemsMass = state.items.map(item => {
+                if (item.id !== action.payload.listItem) {
+                    return item
+                } else {
+                    const newItem = {
+                        id: item.id,
+                        name: item.name,
+                        color: item.color,
+                        tasks: item.tasks.map(task => {
+                            if(task.id === action.payload.id) {
+                                const newTask = {
+                                    id: action.payload.id,
+                                    listItem: action.payload.listItem,
+                                    isCompleted: action.payload.isCompleted,
+                                    task: action.payload.task
                                 }
+                                return newTask
                             } else {
                                 return task
                             }
                         })
+                    }
+                    return newItem
+                }
+            })
 
-                        const newItem =  {...item, tasks: [...newTasksMass]}
+            return {
+                ...state,
+                items: newItemsMass,
+                showAllTasks: [...newItemsMass.map(task => task.tasks)].flat()
+            }
+        }
 
-                        return newItem
+        case DELETE_TASK: {
+
+            const newItemsMass = state.items.map(item => {
+                if(item.id !== action.payload.listItem) {
+                    return item
+                } else {
+                    const newItem = {
+                        id: item.id,
+                        name: item.name,
+                        color: item.color,
+                        tasks: item.tasks.filter(task => task.id !== action.payload.id)
 
                     }
-                })
+                    return newItem
+
+                }
+            })
+
+            return {
+                ...state,
+                items: newItemsMass,
+                showAllTasks: [...newItemsMass.map(task => task.tasks)].flat()
             }
+        }
+
+
+        case CHECK_TASK: {
+
+            const newItemsMass = state.items.map(item => {
+                if(item.id !== action.payload.wasRender) {
+                    return item
+                } else {
+                    const newTasksMass = item.tasks.map(task => {
+                        if(task.id === action.payload.taskId) {
+                            return {
+                                listItem: action.payload.wasRender,
+                                id: task.id,
+                                task: task.task,
+                                isCompleted: !task.isCompleted
+                            }
+                        } else {
+                            return task
+                        }
+                    })
+
+                    const newItem =  {...item, tasks: [...newTasksMass]}
+
+                    return newItem
+
+                }
+            })
+
+            return {
+                ...state,
+                items: newItemsMass,
+                showAllTasks: [...newItemsMass.map(task => task.tasks)].flat()
+
+            }}
         case SHOW_ALL_TASKS:
             return {
                 ...state,
                 isShowAllTasks: true,
                 showAllTasks: [...state.items.map(task => task.tasks)].flat(),
-                // activeListItem: false
             }
 
         default:
@@ -114,10 +209,13 @@ export const reducer = (state = initialState, action) => {
 }
 
 export const setItemsList = (payload) => ({type: SET_ITEMS_LIST, payload})
+export const editItemList = payload => ({type: EDIT_ITEM_LIST, payload})
 export const deleteItemsList = payload => ({type: DELETE_ITEM_LIST, payload})
 export const setActiveItemList = payload => ({type: SET_ACTIVE_ITEM_LIST, payload})
 
 export const setTasks = (payload) => ({type:SET_TASKS, payload})
+export const editTask = payload => ({type:EDIT_TASK, payload})
+export const deleteTask = payload => ({type:DELETE_TASK, payload})
 export const checkTask = (payload) => ({type:CHECK_TASK, payload})
 
 export const showAllTasks = () => ({type:SHOW_ALL_TASKS})
